d6e62d537661164146c8e14c67c45dc6199f6d0c
shi-v-ani-singh <shivanisingh1408203@gmail.com>
2024-06-16 10:00:10 +0530

    first

---
+++ admin/src/Pages/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login'>
+       <div className="loginsignup-container">
+        <h1>{state}</h1>
+        <div className="loginsignup-fields">
+          {state==="Sign Up"?<input type="text" placeholder="Your name" name="username" value={formData.username} onChange={changeHandler}/>:<></>}
+          <input type="email" placeholder="Email address" name="email" value={formData.email} onChange={changeHandler}/>
+          <input type="password" placeholder="Password" name="password" value={formData.password} onChange={changeHandler}/>
+        </div>
+
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+
+        {state==="Login"?
+        <p className="loginsignup-login">Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
+        :<p className="loginsignup-login">Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>}
+
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
